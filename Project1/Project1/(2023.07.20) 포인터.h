b754bebdd95661dd64c4071855aaef5346c8b19f
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-07-21 19:48:59 +0900

    (2023.07.21) 포인터 상수화

---
+++ Project1/Project1/(2023.07.20) 포인터.h
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main()
+{
+#pragma region 주소 연산자
+	// 변수의 주소 값을 반환하는 연산자입니다.
+
+	int data = 100;
+
+	// 메모리 주소는 프로그램이 실행될때마다
+	// 계속 변경됩니다.
+	// printf("data의 주소: %p\n", &data);
+
+	// 데이터의 주소 값은 해당 데이터가 저장된
+	// 메모리의 시작 주소를 의미하며, 메모리의
+	// 공간은 1 byte 크기로 나누어 표현합니다.
+
+#pragma endregion
+
+#pragma region scanf() 함수
+	// 표준 입력 함수로, 여러 종류의 데이터를 다양한
+	// 서식에 맞추어 입력해주는 함수입니다.
+
+	//int input = 0;
+
+	// 입력할 데이터의 서식 지정자, 내가 입력할 변수의 주소
+
+	// 표준 입력함수로 데이토를 입력하게 되면 버퍼에 
+	// 데이터를 보관하였다가 입력하는 순간 버퍼 안의
+	// 내용을 프로그램에 전송합니다.
+	//scanf_s("%d", &input);
+
+	//printf("input 변수의 값 : %d\n", input);
+
+	// while (1)
+	// {
+	// 	scanf_s("%d", &input);
+	// 
+	// 	printf("입력하였습니다...\nn");
+	// }
+
+#pragma endregion
+
+#pragma region 포인터
+
+	// 메모리의 주소값을 저장할 수 있는 변수입니다.
+
+	int value = 100;
+
+	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며
+	// 포인터 변수에 변수의 주소를 저장하게 되면
+	// 해당 변수의 시작 주소를 가리키게 됩니다.
+
+	int* ptr = &value;
+
+	printf("ptr의 값 : %p\n", ptr);
+	printf("value의 주소 값 : %p\n", &value);
+	printf("ptr 변수의 고유 주소 : %p\n", &ptr);
+
+	*ptr = 9999;
+
+	// 포인터가 가리키는 메모리 공간의 자료형은
+	// 알 수 없으므로 포인터가 가리키는 메모리의
+	// 자료형을 선언해주어야 합니다.
+
+	printf("ptr 변수가 가리키는 값 : %d\n", *ptr);
+	printf("value 변수의 값 : %d\n", value);
+
+#pragma endregion
+
+}
--- Project1/Project1/source.cpp
+++ Project1/Project1/source.cpp
@@ -1,71 +1,90 @@
 #include <stdio.h>
+#pragma region 매크로
+	// 프로그램 내에서 특정한 데이터가 문자열로
+	// 정의되고 처리되는 과정입니다.
+
+//#define SIZE 5 // 리터럴 상수
+	// 전처리기란?
+	// 프로그램이 컴파일되기 이전에 프로그램에 대한
+	// 사전 처리하는 과정입니다.
+
+#pragma endregion
 
 int main()
 {
-#pragma region 주소 연산자
-	// 변수의 주소 값을 반환하는 연산자입니다.
+#pragma region 포인터 상수화
 
-	int data = 100;
+	// float data = 99.865f;
+	// int* pointer = NULL;
 
-	// 메모리 주소는 프로그램이 실행될때마다
-	// 계속 변경됩니다.
-	// printf("data의 주소: %p\n", &data);
+	// 포인터 변수의 크기는 중앙처리 장치가
+	// 한번에 처리할  수 있는 크기로 정해지며,
+	// 한번에 처리할 수 있는 크기는
+	// 운영체제에 따라 크기가 결정됩니다.
+	// printf("pointer 변수의 크기 : %d\n", sizeof(pointer));
 
-	// 데이터의 주소 값은 해당 데이터가 저장된
-	// 메모리의 시작 주소를 의미하며, 메모리의
-	// 공간은 1 byte 크기로 나누어 표현합니다.
+	// 포인터 변수를 저장하기 위해 주소값을 저장할
+	// 변수의 자료형과 포인터 변수의 자료형이 일치해야 합니다.
+	// pointer = &data; // ERROR
+
+	// 상수 지시 포인터
+	/*
+	int hp = 100;
+	int mp = 50;
+
+	const int* cPtr = &hp;
+	printf("cPtr이 가리키는 값: %d\n", *cPtr);
+
+	// 포인터 변수를 상수로 선언하여, 포인터 변수가 가리키고
+	// 있는 주소에 존재하는 값을 변경할 수 없습니다.
+	// *cPtr = 999; (ERROR)
+
+	cPtr = &mp;
+	// 포인터로 참조되어 있는 변수는 상수화에 대한 영향을
+	// 받지 않습니다
+
+	mp = 999;
+	printf("cPtr이 가리키는 값: %d\n", *cPtr);
+	*/
+
+	// 포인터 상수
+	/*
+	// 상수 포인터
+	
+	float decimal1 = 5.75f;
+	float decimal2 = 9.99f;
+
+	float* const floatPtr = &decimal1;
+
+	// 포인터 상수는 포인터 변수가 상수화되어 새로운 주솟값을
+	// 저장할 수 없습니다.
+	// floatPtr = &decial2; ERROR
+
+	*floatPtr = 17.342f;
+
+	printf("floatPtr이 가르키는 값 : %f", *floatPtr);
+	*/
 
 #pragma endregion
 
-#pragma region scanf() 함수
-	// 표준 입력 함수로, 여러 종류의 데이터를 다양한
-	// 서식에 맞추어 입력해주는 함수입니다.
+#pragma region 매크로
+	/*
+	// 매크로의 경우 자료형이 존재하지 않으므로
+	// 매모리 공간을 가지고 있지 않습니다.
 
-	//int input = 0;
-
-	// 입력할 데이터의 서식 지정자, 내가 입력할 변수의 주소
-
-	// 표준 입력함수로 데이토를 입력하게 되면 버퍼에 
-	// 데이터를 보관하였다가 입력하는 순간 버퍼 안의
-	// 내용을 프로그램에 전송합니다.
-	//scanf_s("%d", &input);
-
-	//printf("input 변수의 값 : %d\n", input);
-
-	// while (1)
-	// {
-	// 	scanf_s("%d", &input);
-	// 
-	// 	printf("입력하였습니다...\nn");
-	// }
-
-#pragma endregion
-
-#pragma region 포인터
-
-	// 메모리의 주소값을 저장할 수 있는 변수입니다.
-
-	int value = 100;
-
-	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며
-	// 포인터 변수에 변수의 주소를 저장하게 되면
-	// 해당 변수의 시작 주소를 가리키게 됩니다.
-
-	int* ptr = &value;
-
-	printf("ptr의 값 : %p\n", ptr);
-	printf("value의 주소 값 : %p\n", &value);
-	printf("ptr 변수의 고유 주소 : %p\n", &ptr);
-
-	*ptr = 9999;
-
-	// 포인터가 가리키는 메모리 공간의 자료형은
-	// 알 수 없으므로 포인터가 가리키는 메모리의
-	// 자료형을 선언해주어야 합니다.
-
-	printf("ptr 변수가 가리키는 값 : %d\n", *ptr);
-	printf("value 변수의 값 : %d\n", value);
-
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("낚시중...\n");
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("채집중...\n");
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("사냥중...\n");
+	}
+	*/
 #pragma endregion
 
 }
